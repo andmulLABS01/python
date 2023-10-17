18a4978f96f32866a14ae24bc788f8433b99923c
Andrew Mullen <139258459+andmulLABS01@users.noreply.github.com>
2023-10-17 11:41:42 -0400

    Update RunningProcessesCSV.py

--- RunningProcessesCSV.py
+++ RunningProcessesCSV.py
@@ -8,21 +8,21 @@ import csv
 print(list(psutil.Process().as_dict().keys()))
 print(type(list(psutil.Process().as_dict().keys())))
 
-# Now we have the attributes and we learned that they are in a list.
+# Now we have the attributes, and we learned that they are in a list.
 
-# Looking up the values to the attribures, the follwoing attributes will give us the information we need to fill in our CSV file.
-# 'pid', 'name', 'exe', 'cpu_percent','memory_info'
-# psutil has a function called 'psutil.process_iter'.  This function returns an iterator yielding a Process class instance for all running processes on the local machine.
-# This function also allows us to specify attributes, attrs=[], that we need for our output.  We now have enough information to proceeed. 
+# Looking up the values of the attributes will give us the information we need to fill in our CSV file.
+# 'pid', 'name', 'exe', 'cpu_percent', 'memory_info'
+# psutil has a function called 'psutil.process_iter'.  This function returns an iterator, yielding a Process class instance for all running processes on the local machine.
+# This function also allows us to specify attributes, attrs=[], that we need for our output.  We now have enough information to proceed. 
 
 
 # Get a list of all running processes, defining an empty list
 process_list = []
 
-# Since the result of the 'psutil.process_iter' is a list we need to iterate thorugh it using a for loop
+# Since the result of the 'psutil.process_iter' is a list, we need to iterate through it using a for loop
 for p in psutil.process_iter(['pid', 'name', 'exe', 'cpu_percent', 'memory_info']):
 
-    # We need to create a veraiable to plase the irritated data in and append the information to our empty list
+    # We need to create a variable to place the irritated data into and append the information to our empty list
     process_info = p.info
     process_list.append([
         process_info['pid'],
