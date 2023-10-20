a2a51a0ffd2a783d4f8b2618a9588f547437f09b
Andrew Mullen <139258459+andmulLABS01@users.noreply.github.com>
2023-10-20 13:59:41 -0400

    Update NBA_Players.py

--- NBA_Players.py
+++ NBA_Players.py
@@ -1,4 +1,3 @@
-
 #Import modules  os, requests, json, and csv
 import os
 import csv
@@ -19,43 +18,52 @@ headers = {
     'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
   }
 
-# Make the API requestpit
-try:
-    response = requests.get(url, headers=headers, params=querystring)
-
-# Check if the request was successful
-    if response.status_code == 200:
-        try:
-            player_data = response.json()
-
-            # Specify the name of the CSV file.
-            csv_file = 'nba_players.csv'
-
-            # Extract and format the data
-            formatted_data = []
-            for player in player_data['data']:
-                formatted_data.append({
-                    'Player ID': player['id'],
-                    'First Name': player['first_name'],
-                    'Last Name': player['last_name'],
-                    'Position': player['position'],
-                    'Team Name': player['team']['full_name']
-                })
-
-            # Write the data to a CSV file
-            with open(csv_file, 'w', newline='') as csvfile:
-                fieldnames = ['Player ID', 'First Name', 'Last Name', 'Position', 'Team Name']
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                writer.writeheader()
-                writer.writerows(formatted_data)
-
-            print(f'Data has been saved to {csv_file}')
-        # Add additional error handling     
-        except json.JSONDecodeError:
-            print("Error: The response does not contain valid JSON data.")
-    else:
-        print(f'Failed to retrieve data. Status code: {response.status_code}')
-except requests.exceptions.RequestException as e:
-    print(f'Error: {e}')
 
 
+# Set next_page to true so the while loop can begin
+
+next_page = True  
+while next_page is not None:
+
+    # Make the API requestpit
+    try:
+        response = requests.get(url, headers=headers, params=querystring)
+
+    # Check if the request was successful
+        if response.status_code == 200:
+            try:
+                player_data = response.json()
+
+                # Specify the name of the CSV file.
+                csv_file = 'nba_players.csv'
+                
+                # Extract and format the data
+                formatted_data = []
+                for player in player_data['data']:
+                    formatted_data.append({
+                        'Player ID': player['id'],
+                        'First Name': player['first_name'],
+                        'Last Name': player['last_name'],
+                        'Position': player['position'],
+                        'Team Name': player['team']['full_name']
+                    })
+                # Assigns next page to see if we need to iterate again
+                next_page = player_data['meta']['next_page']
+                # Assigns next page number to call the next page
+                querystring['page'] = f"{next_page}"
+                
+                # Save the data as a CSV file.
+                with open(csv_file, 'w', newline='') as csvfile:
+                    fieldnames = ['Player ID', 'First Name', 'Last Name', 'Position', 'Team Name']
+                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                    writer.writeheader()
+                    writer.writerows(formatted_data)
+
+                print(f'Data has been saved to {csv_file}')
+            # Add additional error handling     
+            except json.JSONDecodeError:
+                print("Error: The response does not contain valid JSON data.")
+        else:
+            print(f'Failed to retrieve data. Status code: {response.status_code}')
+    except requests.exceptions.RequestException as e:
+        print(f'Error: {e}')
